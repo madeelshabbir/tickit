ad4342a63f325ba86d136d393a17c079f2ac4b03
Adeel Shabbir <madeelshabbirs@gmail.com>
2022-10-03 23:57:38 +0500

    B-2: Normalize error response
    * auth/package-lock.json: Auto-updated after installation of express-async-errors.
    * auth/package.json: Added express-async-errors package.
    * auth/src/errors/custom_error.ts: Created error abstract error class.
    * auth/src/errors/database_connection_error.ts: Created error class for database_connection.
    * auth/src/errors/not-found-error.ts: Created error class for not found error.
    * auth/src/errors/request_validation_error.ts: Created error class for request validation.
    * auth/src/index.ts: Created wild card endpoint throwing not found error.
    * auth/src/middlewares/error-handler.ts: Created error handler middleware.
    * auth/src/routes/sign-up.ts: Threw request validation and database connection errors.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/sign-in';
 import { signOutRouter } from './routes/sign-out';
 import { signUpRouter } from './routes/sign-up';
+import { errorHanlder } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -12,6 +15,12 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHanlder);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
