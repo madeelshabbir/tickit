843f472d2e168096c2560954879babb6fef2e9e8
Adeel Shabbir <madeelshabbirs@gmail.com>
2022-11-11 22:38:53 +0500

    B-3: Create User Model
    * auth/package-lock.json: Auto-updated after installation of mongoose.
    * auth/package.json: Added mongoose package.
    * auth/src/errors/bad-request-error.ts: Created custom error class.
    * auth/src/errors/custom_error.ts: Updated constructor.
    * auth/src/errors/database-connection-error.ts: Updated constructor and renamed file.
    * auth/src/errors/not-found-error.ts: Updated constructor.
    * auth/src/errors/request-validation-error.ts: Updated constructor and renamed file.
    * auth/src/index.ts: Added database connection code.
    * auth/src/models/user.ts: Created User Model.
    * auth/src/routes/sign-up.ts: Updated sign-up api.
    * auth/src/services/password.ts: Created service to hash and compare password.
    * infra/k8s/auth-mongo-depl.yaml: Created deployment and service for mongo.

---
+++ auth/src/errors/bad-request-error.ts
@@ -0,0 +1,15 @@
+import { CustomError } from "./custom_error";
+
+export class BadRequestError extends CustomError {
+  statusCode = 400;
+
+  constructor(public message: string) {
+    super(message);
+
+    Object.setPrototypeOf(this, BadRequestError.prototype);
+  }
+
+  serialize() {
+    return [{ message: this.message }]
+  }
+};
