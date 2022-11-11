1390d04c2189f2a80bfed031f2df3ad4d60714cc
Adeel <54504931+madeelshabbir@users.noreply.github.com>
2022-11-11 22:51:18 +0500

    B-3: Create User Model (#4)
    * auth/package-lock.json: Auto-updated after installation of mongoose.
    * auth/package.json: Added mongoose package.
    * auth/src/errors/bad-request-error.ts: Created custom error class.
    * auth/src/errors/custom_error.ts: Updated constructor.
    * auth/src/errors/database-connection-error.ts: Updated constructor and renamed file.
    * auth/src/errors/not-found-error.ts: Updated constructor.
    * auth/src/errors/request-validation-error.ts: Updated constructor and renamed file.
    * auth/src/index.ts: Added database connection code.
    * auth/src/models/user.ts: Created User Model.
    * auth/src/routes/sign-up.ts: Updated sign-up api.
    * auth/src/services/password.ts: Created service to hash and compare password.
    * infra/k8s/auth-mongo-depl.yaml: Created deployment and service for mongo.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/sign-in';
 import { signOutRouter } from './routes/sign-out';
@@ -22,6 +23,18 @@ app.all('*', async () => {
 
 app.use(errorHanlder);
 
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to Mongodb');
+  }
+  catch (err) {
+    console.log(err);
+  }
+};
+
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
 });
+
+start();
