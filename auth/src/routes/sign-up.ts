da2f4b724bc681fe2a17a68d91929767898747fb
Adeel <54504931+madeelshabbir@users.noreply.github.com>
2022-11-11 22:51:18 +0500

    B-3: Create User Model (#4)
    * auth/package-lock.json: Auto-updated after installation of mongoose.
    * auth/package.json: Added mongoose package.
    * auth/src/errors/bad-request-error.ts: Created custom error class.
    * auth/src/errors/custom_error.ts: Updated constructor.
    * auth/src/errors/database-connection-error.ts: Updated constructor and renamed file.
    * auth/src/errors/not-found-error.ts: Updated constructor.
    * auth/src/errors/request-validation-error.ts: Updated constructor and renamed file.
    * auth/src/index.ts: Added database connection code.
    * auth/src/models/user.ts: Created User Model.
    * auth/src/routes/sign-up.ts: Updated sign-up api.
    * auth/src/services/password.ts: Created service to hash and compare password.
    * infra/k8s/auth-mongo-depl.yaml: Created deployment and service for mongo.

--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
-import { DatabaseConnectionError } from '../errors/database_connection_error';
-import { RequestValidationError } from '../errors/request_validation_error';
+import { BadRequestError } from '../errors/bad-request-error';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { User } from '../models/user';
 
 const router = express.Router();
 
@@ -13,7 +14,7 @@ router.post('/api/users/sign-up', [
     .trim()
     .isLength({ min: 4, max: 20 })
     .withMessage('Password must be between 4 and 20 characters')
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -22,10 +23,16 @@ router.post('/api/users/sign-up', [
 
   const { email, password } = req.body;
 
-  console.log('Creating a user...');
-  throw new DatabaseConnectionError();
+  const existingUser = await User.findOne({ email });
 
-  res.send({});
+  if (existingUser) {
+    throw new BadRequestError('Email in use');
+  }
+
+  const user = User.build({ email, password });
+  await user.save();
+
+  res.status(201).send(user);
 });
 
 export { router as signUpRouter };
