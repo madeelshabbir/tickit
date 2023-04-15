03310b57300b1654b61a37d7147528ab71a50fd8
Adeel Shabbir <54504931+madeelshabbir@users.noreply.github.com>
2023-04-15 20:51:29 +0500

    B-5: Write isolated tests for auth service (#7)
    * package-lock.json: Auto-updated after new package installing.
    * package.json: Added new npm packages.
    * src/app.ts: Moved the express app code from index.ts to this file.
    * src/index.ts: Removed express app code.
    * src/routes/__test__/current-user.test.ts: Created tests for current-user endpoint.
    * src/routes/__test__/sign-in.test.ts: Created tests for sign-in endpoint.
    * src/routes/__test__/sign-out.test.ts: Created tests for sign-out endpoint.
    * src/routes/__test__/sign-up.test.ts: Created tests for sign-up endpoint.
    * src/routes/current-user.ts: Removed extra middleware.
    * src/test/setup.ts: Created setup file for test configurations and helpers.

---
+++ auth/src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/sign-in';
+import { signOutRouter } from './routes/sign-out';
+import { signUpRouter } from './routes/sign-up';
+import { errorHanlder } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+  signed: false,
+  secure: process.env.NODE_ENV !== 'test'
+}));
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHanlder);
+
+export { app };
