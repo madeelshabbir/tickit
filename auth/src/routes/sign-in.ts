f737bbb9c18c2c2f5b609d6475d411d46a7b8676
Adeel Shabbir <madeelshabbirs@gmail.com>
2024-01-31 18:47:08 +0500

    B-7: Create and use reusable npm package
    * auth/package-lock.json: Auto updated after installation of custom package.
    * auth/package.json: Installed @tickitcore/common custom package.
    * auth/src/app.ts: Updated import to import common code from @tickitcore/common.
    * auth/src/routes/current-user.ts: Updated import to import common code from @tickitcore/common.
    * auth/src/routes/sign-in.ts: Updated import to import common code from @tickitcore/common.
    * auth/src/routes/sign-up.ts: Updated import to import common code from @tickitcore/common.
    * client/components/header.js: Updated site title.
    * client/next.config.js: Updated the depreciated key.
    * common/.gitignore: Created file to ignore extra files in repository.
    * common/package-lock.json: Auto updated after installation of packages.
    * common/package.json: Created file for package configrations.
    * common/src/errors/bad-request-error.ts: Moved it from auth to common.
    * common/src/errors/custom-error.ts: Moved it from auth to common.
    * common/src/errors/database-connection-error.ts: Moved it from auth to common.
    * common/src/errors/not-authorized-error.ts: Moved it from auth to common.
    * common/src/errors/not-found-error.ts: Moved it from auth to common.
    * common/src/errors/request-validation-error.ts: Moved it from auth to common.
    * common/src/index.ts: Created endpoint file for the package to export all classes and methods.
    * common/src/middlewares/current-user.ts: Moved it from auth to common.
    * common/src/middlewares/error-handler.ts: Moved it from auth to common.
    * common/src/middlewares/require-auth.ts: Moved it from auth to common.
    * common/src/middlewares/validate-request.ts: Moved it from auth to common.
    * common/tsconfig.json: Created file for typescript configurations.

--- auth/src/routes/sign-in.ts
+++ auth/src/routes/sign-in.ts
@@ -1,10 +1,9 @@
 import express, { type Request, type Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
+import { BadRequestError, validateRequest } from '@tickitcore/common';
 
-import { validateRequest } from '../middlewares/validate-request';
 import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
 import { Password } from '../services/password';
 
 const router = express.Router();
