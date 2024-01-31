a8b41aa6a27eddb36d9e97a9363de0240467bb13
Adeel Shabbir <54504931+madeelshabbir@users.noreply.github.com>
2024-01-31 19:03:42 +0500

    B-7: Create and use reusable npm package (#9)
    * auth/package-lock.json: Auto updated after installation of custom package.
    * auth/package.json: Installed @tickitcore/common custom package.
    * auth/src/app.ts: Updated import to import common code from @tickitcore/common.
    * auth/src/routes/current-user.ts: Updated import to import common code from @tickitcore/common.
    * auth/src/routes/sign-in.ts: Updated import to import common code from @tickitcore/common.
    * auth/src/routes/sign-up.ts: Updated import to import common code from @tickitcore/common.
    * client/components/header.js: Updated site title.
    * client/next.config.js: Updated the depreciated key.
    * common/.gitignore: Created file to ignore extra files in repository.
    * common/package-lock.json: Auto updated after installation of packages.
    * common/package.json: Created file for package configrations.
    * common/src/errors/bad-request-error.ts: Moved it from auth to common.
    * common/src/errors/custom-error.ts: Moved it from auth to common.
    * common/src/errors/database-connection-error.ts: Moved it from auth to common.
    * common/src/errors/not-authorized-error.ts: Moved it from auth to common.
    * common/src/errors/not-found-error.ts: Moved it from auth to common.
    * common/src/errors/request-validation-error.ts: Moved it from auth to common.
    * common/src/index.ts: Created endpoint file for the package to export all classes and methods.
    * common/src/middlewares/current-user.ts: Moved it from auth to common.
    * common/src/middlewares/error-handler.ts: Moved it from auth to common.
    * common/src/middlewares/require-auth.ts: Moved it from auth to common.
    * common/src/middlewares/validate-request.ts: Moved it from auth to common.
    * common/tsconfig.json: Created file for typescript configurations.

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,7 +1,5 @@
 import express from 'express';
-
-import { currentUser } from '../middlewares/current-user';
-import { requireAuth } from '../middlewares/require-auth';
+import { currentUser } from '@tickitcore/common';
 
 const router = express.Router();
 
